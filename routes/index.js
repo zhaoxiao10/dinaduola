1d67da0810782ffc54e7d41128b0ab7239892c3a
zhaoyan <zhaoxiao10@gmail.com>
2016-12-16 01:27:27 +0800

    add mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+//db
+var db = require('mongoskin').db('mongodb://dinaduola:dinaduola@localhost:27017/dinaduola');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.collection('user').find({"username":"pengji"}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.render('index', { title: result[0].username });
+  });
 });
 
 module.exports = router;
